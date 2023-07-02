4837ea03690de885d571a48485e90b262043060a
KING787746 <136891895+KING787746@users.noreply.github.com>
2023-07-02 19:53:56 +0530

    Update info.py

--- info.py
+++ info.py
@@ -42,8 +42,8 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://kanhaiyalalgurjar9393:stark@stark.vl461dy.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "STARK")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://kevinkking7877:stark2@stark2.dmzh2ix.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "STARK2")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 #maximum search result buttos count in number#
